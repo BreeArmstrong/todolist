8021b45a0ee69da34dd0d192a4b6f1170f30e0a0
Brianna Grizzell <breegrizzell@gmail.com>
2020-02-02 16:41:10 -0800

    third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Todo = (props) => {
    <li className={`todo ${complete ? 'complete' : ''}`}>
     <input data-id={id} type="checkbox" checked={complete} onChange={handleComplete}/>
     <span className={complete ? 'complete' : ''}>{name}</span>
-    <a data-id={id} href="#">Delete</a>
+    <a data-id={id} onClick={handleDelete} href="#">Delete</a>
     <div>{JSON.stringify(handleDelete)}</div>
    </li>
  );
@@ -20,6 +20,21 @@ function App() {
  const [state, setState] = useState({});
  //For displaying specific types
  const [showType, setShowType] = useState('all');
+ 
+ const handleEnter = e => {
+  if(e.key === 'Enter') {
+   const id = uuid.v4();
+   setState({
+    ...state,
+    [id]: {
+     name: e.target.value,
+     id,
+     complete: false
+    }
+   });
+   e.target.value = '';
+  }
+ };
  
  const handleDelete = e => {
   const {id} = e.target.dataset;
@@ -39,7 +54,11 @@ function App() {
  
  const handleShowType = e => {
   const {type} = e.target.dataset;
-  console.log(type);
+  switch (type) {
+  case (type === 'active'){
+   return 
+  }
+  }
   setShowType(type);
  };
  
@@ -53,12 +72,12 @@ function App() {
    
  return(
    <section className={'todos'}>
-    <input type="text" placeholder={"Todo Here"}/>
+    <input type="text" placeholder={"Todo Here"} onKeyDown={handleEnter}/>
     <h1>Todos</h1>
     <ul>
-     <li onClick={}>Show Active</li>
-     <li onClick={}>Show Complete</li>
-     <li onClick={}>Show All</li>
+     <li> <a href="#" data-type="active" onClick={handleShowType}>Show Active</a></li>
+     <li> <a href="#" data-type="complete" onClick={handleShowType}>Show Complete</a></li>
+     <li> <a href="#" data-type="all" onClick={handleShowType}>Show All</a></li>
     </ul>
     <ul className={`todos ${showType}`}>
      {renderedTodos}
