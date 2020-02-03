9ce49bef157d2244c04918bc93f4c52019816fc4
Brianna Grizzell <breegrizzell@gmail.com>
2020-02-02 16:08:52 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, {useState} from 'react';
 import uuid from 'uuid';
-import Todo from './components/Todo';
 import './App.css';
 
+const Todo = (props) => {
+ const {todo, handleDelete} = props;
+ const {id, name, complete} = todo;
+ return(
+   <li className={}>
+    <input data-id={id} type="checkbox" checked={complete}/>
+    <span className={}>{name}</span>
+    <a data-id={id} href="#">Delete</a>
+    <div>{JSON.stringify(handleDelete)}</div>
+   </li>
+ );
+};
+
 function App() {
- return <div>Hello World~</div>;
+ return;
 }
 
 export default App;
