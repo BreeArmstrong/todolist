3efd6d6e4224717eb49d0200545587dbe30bc759
Brianna Grizzell <breegrizzell@gmail.com>
2020-02-02 16:26:05 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import uuid from 'uuid';
 import './App.css';
 
 const Todo = (props) => {
- const {todo, handleDelete} = props;
+ const {todo, handleDelete, handleComplete} = props;
  const {id, name, complete} = todo;
  return(
-   <li className={}>
-    <input data-id={id} type="checkbox" checked={complete}/>
-    <span className={}>{name}</span>
+   <li className={`todo ${complete ? 'complete' : ''}`}>
+    <input data-id={id} type="checkbox" checked={complete} onChange={handleComplete}/>
+    <span className={complete ? 'complete' : ''}>{name}</span>
     <a data-id={id} href="#">Delete</a>
     <div>{JSON.stringify(handleDelete)}</div>
    </li>
@@ -16,7 +16,55 @@ const Todo = (props) => {
 };
 
 function App() {
- return;
+ // For handling the states in the todo array
+ const [state, setState] = useState({});
+ //For displaying specific types
+ const [showType, setShowType] = useState('all');
+ 
+ const handleDelete = e => {
+  const {id} = e.target.dataset;
+  delete state[id];
+  setState({
+   ...state
+  });
+ };
+ 
+ const handleComplete = e => {
+  const {id} = e.target.dataset;
+  state[id].complete = !state[id].complete;
+  setState({
+   ...state
+  });
+ };
+ 
+ const handleShowType = e => {
+  const {type} = e.target.dataset;
+  console.log(type);
+  setShowType(type);
+ };
+ 
+ const renderedTodos = Object.values(state)
+   .map(t =>
+     <Todo handleDelete={handleDelete}
+           handleComplete={handleComplete}
+           key={t.id}
+           todo={t} />
+   );
+   
+ return(
+   <section className={'todos'}>
+    <input type="text" placeholder={"Todo Here"}/>
+    <h1>Todos</h1>
+    <ul>
+     <li onClick={}>Show Active</li>
+     <li onClick={}>Show Complete</li>
+     <li onClick={}>Show All</li>
+    </ul>
+    <ul className={`todos ${showType}`}>
+     {renderedTodos}
+    </ul>
+   </section>
+ );
 }
 
 export default App;
